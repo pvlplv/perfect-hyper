bbe76afcfc9ba9c3e40fbff53907c83ed0a141d0
Pavel <36270842+pvlplv@users.noreply.github.com>
2020-10-23 16:40:56 +0300

    minor refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const perfectForegoundColor = "#998FAA"
 const perfectCursorColor = "rgba(153,143,170,0.8)"
+const perfectForegoundColor = "#998FAA"
 const perfectBackgroundColor = "#25252E"
 const perfectSelectionColor = "rgba(37,37,46,0.8)"
 
-exports.decorateConfig = (config) => {
+export function decorateConfig(config) {
   return Object.assign({}, config, {
     cursorColor: perfectCursorColor,
     cursorAccentColor: perfectBackgroundColor,
